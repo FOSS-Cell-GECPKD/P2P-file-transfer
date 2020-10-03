a9caadf4aa9e610b15b1d481b29f0ce4a4a09412
Sankar Vinayak <sankarvinayak@github.com>
2020-10-03 21:47:36 +0530

    Rewritten server

--- client.py
+++ client.py
@@ -1,29 +1,40 @@
-import socket
+def receive_file():
+    import socket
+    import os
+    from requests import get
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+    host=input(str("Enter the host address of the senter : "))
+    port=8080
+    s.connect((host,port))
+    print(" Connected to the host... ")
+    filename=s.recv(1024)
+    filename=filename.decode()
+    filename=os.path.basename(filename)
+    filesize=s.recv(1024)
+    filesize=int(filesize.decode())
+    print("Do you want to download % s of size % s" % (filename, filesize))
+    ans=input(str(("\nY-Yes N-No->")))
+    if(ans =='y' or ans == 'Y'):
+        ans = 'Y'
+    elif(ans =='N' or ans == 'n'):
+        ans = 'N'
+    s.send(ans.encode())
+    if(ans =='Y'):
+        with open(filename, 'wb') as file:
+            chunk_size=1024
+            chunk_file=s.recv(chunk_size)
+            file.write(chunk_file)
+            totalrecv=len(chunk_file)
+            while totalrecv<filesize:
+                chunk_file=s.recv(chunk_size)
+                file.write(chunk_file)
+                totalrecv+=len(chunk_file)
+                print(totalrecv*100/filesize)
+        
+        file.close()
 
-s=socket.socket()
-host=input(str("Enter the host address of the senter : "))
-port=8080
-s.connect((host,port))
-print(" Connected to the host... ")
-filename=s.recv(1024)
-filename=filename.decode()
-size=s.recv(1024)
-size=int(size.decode())
-print("name of the file = "+filename)
-print("This may take few seconds...")
-with open(filename, 'wb') as file:
-    chunk_size=1024
-    chunk_file=s.recv(chunk_size)
-    file.write(chunk_file)
-    file_size=len(chunk_file)
-    while file_size < size:
-        chunk_file=s.recv(chunk_size)
-        file.write(chunk_file)
-        file_size+=len(chunk_file)
-    
-"""file=open(filename,'wb')
-file_data=s.recv(25*1024)
-file.write(file_data)
-file.close()"""
+        print("File has been recived.")
+    else:
+        print("!!Exiting!!")    
+    s.close()
 
-print("File has been recived.")
