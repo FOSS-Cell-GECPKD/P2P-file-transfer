d43b50c92d08ab541006e9765e4426e1ee55ebcd
Sankar Vinayak E P <64238983+sankarvinayak@users.noreply.github.com>
2020-10-24 11:02:33 +0530

    reformat

--- peer.py
+++ peer.py
@@ -1,22 +1,25 @@
 import send
 import recive
 
+
 def menu():
-    choice=input("""
+    choice = input("""
                 S: Send a file
                 R: Receive a file
                 Please enter your choice (S/R):""")
-    if choice=="S" or choice=="s":
+    if choice == "S" or choice == "s":
         send.send_file()
-    elif choice=="R" or choice=="r":
+    elif choice == "R" or choice == "r":
         recive.receive_file()
     else:
         print("You must only select either S or R")
         print("please try again")
         menu()
 
+
 def main():
     menu()
 
-if __name__ =="__main__":
+
+if __name__ == "__main__":
     main()
--- recive.py
+++ recive.py
@@ -1,57 +1,63 @@
 import socket
 import os
-from requests import get
+
+
+# from requests import get
 def yesorno(s):
-    ans=input(str(("\nY-Yes N-No->")))
-    if(ans =='y' or ans == 'Y'):
-        ans='Y'
+    ans = input(str("\nY-Yes N-No->"))
+    if ans == 'y' or ans == 'Y':
+        ans = 'Y'
         s.send(ans.encode())
         file(s)
-    elif(ans =='N' or ans == 'n'):
+    elif ans == 'N' or ans == 'n':
         ans = 'N'
         s.send(ans.encode())
         s.close()
+
+
 def connection():
     try:
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
-        host=input(str("Enter the host address of the senter : "))
-        port=8080
-        s.connect((host,port))
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        host = input(str("Enter the host address of the senter : "))
+        port = 8080
+        s.connect((host, port))
         print(" Connected to the host... ")
         return s
     except:
         print("Connection error do you want to try again?")
-        ans=input(str(("\nY-Yes N-No->")))
-        if(ans =='y' or ans == 'Y'):
+        ans = input(str("\nY-Yes N-No->"))
+        if ans == 'y' or ans == 'Y':
             file(connection())
-        elif(ans =='N' or ans == 'n'):
+        elif ans == 'N' or ans == 'n':
             exit()
+
+
 def file(s):
-    filename=s.recv(1024)
-    filename=filename.decode()
-    filename=os.path.basename(filename)
-    filesize=s.recv(1024)
-    filesize=int(filesize.decode())
+    filename = s.recv(1024)
+    filename = filename.decode()
+    filename = os.path.basename(filename)
+    filesize = s.recv(1024)
+    filesize = int(filesize.decode())
     print("Do you want to download % s of size % s" % (filename, filesize))
-    ans=input(str(("\nY-Yes N-No->")))
-    if(ans =='y' or ans == 'Y'):
+    ans = input(str("\nY-Yes N-No->"))
+    if ans == 'y' or ans == 'Y':
         ans = 'Y'
-    elif(ans =='N' or ans == 'n'):
+    elif ans == 'N' or ans == 'n':
         ans = 'N'
     s.send(ans.encode())
-    if(ans =='Y'):
-        filename = "Recived"+filename
+    if ans == 'Y':
+        filename = "Recived" + filename
         with open(filename, 'wb') as file:
-            chunk_size=1024
-            chunk_file=s.recv(chunk_size)
+            chunk_size = 1024
+            chunk_file = s.recv(chunk_size)
             file.write(chunk_file)
-            totalrecv=len(chunk_file)
-            while totalrecv<filesize:
-                chunk_file=s.recv(chunk_size)
+            totalrecv = len(chunk_file)
+            while totalrecv < filesize:
+                chunk_file = s.recv(chunk_size)
                 file.write(chunk_file)
-                totalrecv+=len(chunk_file)
-                print(totalrecv*100/filesize)
-        
+                totalrecv += len(chunk_file)
+                print(totalrecv * 100 / filesize)
+
         file.close()
 
         print("File has been recived.")
@@ -59,5 +65,7 @@ def file(s):
         print("!!Exiting!!")
     print("Do yo want to continue?")
     yesorno(s)
+
+
 def receive_file():
     file(connection())
