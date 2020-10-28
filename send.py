0b3cb29fcbac26a8c716a36c15409789b2c5a509
Sankar Vinayak E P <64238983+sankarvinayak@users.noreply.github.com>
2020-10-28 21:15:52 +0530

    requested changes

--- send.py
+++ send.py
@@ -9,10 +9,7 @@ def get_ip():  # Used to get the ip address and host name for creating a network
     print("Host name:" + ip)
 
     try:
-        ip = ([l for l in (
-            [ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if not ip.startswith("127.")][:1], [
-                [(s.connect(('8.8.8.8', 53)), s.getsockname()[0], s.close()) for s in
-                 [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]]) if l][0][0])
+        ip = ([l for l in ([ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if not ip.startswith("127.")][:1], [[(s.connect(('8.8.8.8', 53)), s.getsockname()[0], s.close()) for s in[socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]]) if l][0][0])
         print("Local ip:" + ip)
     except:
         print("Not connected to any network")
